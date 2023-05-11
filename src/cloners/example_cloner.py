50e178737d1ccf7249f266e5b0b786289e0f1187
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2023-05-10 23:33:13 -0400

    Update example, drop _NAME

--- src/cloners/example_cloner.py
+++ src/cloners/example_cloner.py
@@ -6,8 +6,7 @@ In order to add a new cloner :
 
     1. Create a new cloner class
     2. Set cls._ACTIVE attribute to True
-    3. Set cls._NAME attribute to some string
-    4. Add string used for cls._NAME to config attribute 'enabled_cloners'
+    3. Add the class name to config.yaml attribute 'enabled_cloners'
 
 Now the cloner manager should find it
 """
@@ -15,7 +14,6 @@ Now the cloner manager should find it
 
 class ExampleCloner:
     _ACTIVE = True
-    _NAME = 'Example'
 
     def authenticate(self):
         """
